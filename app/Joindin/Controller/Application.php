da6a7b26f11d1e7c15cb6c81cbbce54ce042057f
Eric Hogue <eric@erichogue.ca>
2013-04-09 07:50:20 -0400

    Changed the messages for people attending an event

--- app/Joindin/Controller/Application.php
+++ app/Joindin/Controller/Application.php
@@ -15,6 +15,9 @@ class Application extends Base
         $hot_events      = $event_collection->getCollection(5, 1, 'hot');
         $upcoming_events = $event_collection->getCollection(5, 1, 'upcoming');
 
+        array_walk($hot_events, array($this, 'add_attending_message'));
+        array_walk($upcoming_events, array($this, 'add_attending_message'));
+
         echo $this->application->render(
             'Application/index.html.twig',
             array(
@@ -29,4 +32,53 @@ class Application extends Base
         $_SESSION['access_token'] = $this->application->request()->params('access_token');
         $this->application->redirect('/');
     }
+
+    protected function add_attending_message($event) {
+        $message = $this->get_beginning_of_attending_message((int) $event->attendee_count);
+
+        if ($event->attending) {
+            $message .= '(including you) ';
+        }
+
+        $message .= $this->get_end_of_attending_message($event);
+
+        $event->attending_message = $message;
+    }
+
+
+    protected function get_beginning_of_attending_message($attendee_count) {
+        $message = $attendee_count . ' ';
+        if (1 == $attendee_count) {
+            $message .= 'person ';
+        } else {
+            $message .= 'people ';
+        }
+
+        return $message;
+    }
+
+    protected function get_end_of_attending_message($event) {
+        $are = 'are';
+        if (1 == $event->attendee_count) {
+            $are = 'is';
+        }
+
+        if ($this->isPastEvent($event)) {
+            $message = 'attended.';
+        } else {
+            $message = $are . ' attending.';
+        }
+
+        return $message;
+    }
+
+    protected function isPastEvent($event) {
+        $endDate = \DateTime::createFromFormat(\DateTime::ISO8601, $event->end_date);
+        $now = new \DateTime(null, $endDate->getTimezone());
+        $now->setTime(0, 0, 0);
+
+        return ($endDate < $now);
+    }
+
+
 }
