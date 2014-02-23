4e4bbb6e9379dd82c6dc7fe83a6b1336dab92d10
Kevin <github@magicmonkey.org>
2014-02-23 20:36:17 +0000

    Removed the (now old style) event comments test, but added some other event tests to maintain karma

--- tests/Model/EventTest.php
+++ tests/Model/EventTest.php
@@ -11,23 +11,114 @@ class EventTest extends \PHPUnit_Framework_TestCase
     {
         // Not used at the moment, but it's here for future use when we
         // want to provide data to the class
-        $this->eventData = new \stdClass();
+        $this->eventData = new \stdClass($data);
+		$this->eventData->name                 = "Test event name";
+		$this->eventData->icon                 = "Test event icon";
+		$this->eventData->start_date           = "Test event start date";
+		$this->eventData->end_date             = "Test event end date";
+		$this->eventData->location             = "Test event location";
+		$this->eventData->description          = "Test event description";
+		$this->eventData->tags                 = "Test event tags";
+		$this->eventData->latitude             = "Test event latitude";
+		$this->eventData->longitude            = "Test event longitude";
+		$this->eventData->href                 = "Test event href";
+		$this->eventData->attendee_count       = "Test event attendee count";
+		$this->eventData->event_comments_count = "Test event event comments count";
+		$this->eventData->comments_uri         = "Test event comments uri";
+		$this->eventData->talks_uri            = "Test event talks uri";
+		$this->eventData->uri                  = "Test event uri";
+		$this->eventData->verbose_uri          = "Test event verbose uri";
+		$this->eventData->attending            = "Test event attending";
     }
 
-    public function testEventComments()
+    public function testBasicEventData()
     {
         $event = new Event($this->eventData);
 
-        // Comments should be null at first: we use a setter to set them
         $this->assertEquals(
-            $event->getComments(),
-            null
+            $event->getName(),
+            "Test event name"
         );
 
-        $event->setComments('modified comment data');
         $this->assertEquals(
-            $event->getComments(),
-            'modified comment data'
+            $event->getIcon(),
+            "Test event icon"
         );
+
+        $this->assertEquals(
+            $event->getStartDate(),
+            "Test event start date"
+        );
+
+        $this->assertEquals(
+            $event->getEndDate(),
+            "Test event end date"
+        );
+
+        $this->assertEquals(
+            $event->getLocation(),
+            "Test event location"
+        );
+
+        $this->assertEquals(
+            $event->getDescription(),
+            "Test event description"
+        );
+
+        $this->assertEquals(
+            $event->getTags(),
+            "Test event tags"
+        );
+
+        $this->assertEquals(
+            $event->getLatitude(),
+            "Test event latitude"
+        );
+
+        $this->assertEquals(
+            $event->getLongitude(),
+            "Test event longitude"
+        );
+
+        $this->assertEquals(
+            $event->getHref(),
+            "Test event href"
+        );
+
+        $this->assertEquals(
+            $event->getAttendeeCount(),
+            "Test event attendee count"
+        );
+
+        $this->assertEquals(
+            $event->getCommentsCount(),
+            "Test event event comments count"
+        );
+
+        $this->assertEquals(
+            $event->getCommentsUri(),
+            "Test event comments uri"
+        );
+
+        $this->assertEquals(
+            $event->getTalksUri(),
+            "Test event talks uri"
+        );
+
+        $this->assertEquals(
+            $event->getUri(),
+            "Test event uri"
+        );
+
+        $this->assertEquals(
+            $event->getVerboseUri(),
+            "Test event verbose uri"
+        );
+
+        $this->assertEquals(
+            $event->isAttending(),
+            "Test event attending"
+        );
+
     }
 }
