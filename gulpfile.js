f35089dd08dabdc50bf6f25cc3f3d9896f4e1caa
Davey Shafik <me@daveyshafik.com>
2014-06-10 12:19:49 -0400

    Add a header to state files are generated

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,18 @@ var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var rename = require( 'gulp-rename' )
 var clean = require( 'gulp-clean' )
+var header = require( 'gulp-header' )
 
-gulp.task('default', ['usemin', 'rename', 'clean']);
+gulp.task('default', ['usemin', 'rename', 'add-comments', 'clean']);
 
 gulp.task('usemin', function() {
-    return gulp.src('./app/templates/layout.src.twig')
+    return gulp.src('app/templates/layout.src.twig')
         .pipe(usemin({
-            assetsDir: './web',
+            assetsDir: 'web',
             css: [minifyCss(), 'concat'],
             js: [uglify(), 'concat']
         }))
-        .pipe(gulp.dest('./web'));
+        .pipe(gulp.dest('web'));
 });
 
 gulp.task('rename', ['usemin'], function() {
@@ -22,6 +23,20 @@ gulp.task('rename', ['usemin'], function() {
         .pipe(rename("layout.html.twig"))
         .pipe(gulp.dest('app/templates'));
 });
+
+gulp.task('add-comments', ['rename'], function() {
+    gulp.src('app/templates/layout.html.twig')
+        .pipe(header("<!-- This file is generated — do not edit by hand! -->\n"))
+        .pipe(gulp.dest('app/templates'));
+
+    gulp.src('web/js/site.js')
+        .pipe(header("/* This file is generated — do not edit by hand! */\n"))
+        .pipe(gulp.dest('web/js'));
+
+    gulp.src('web/css/site.css')
+        .pipe(header("/* This file is generated — do not edit by hand! */\n"))
+        .pipe(gulp.dest('web/css'));
+})
 
 gulp.task('clean', ['rename'], function() {
     return gulp.src('web/layout.src.twig')
