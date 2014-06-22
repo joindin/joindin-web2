24864c0f2285fbf5a8a7ababa9ffce7ee8997a02
Mike van Riel <me@mikevanriel.com>
2014-06-22 10:19:29 +0200

    Add form to submit new Events
    In order to be able to submit events to the API we need a
    form to take the event properties, validate those, send them
    to the API and redirect to the details page for that event
    if this all succeeds.
    
    In this commit I have
    
    - added the form definition and template
    - changed the event controller to construct the form instance
    - changed the event controller to validate the form
    - changed the event controller to save the event to the API and
    - redirect if the event was submitted correctly to its detail page
    
    Some styling was added so that the event form looks passable; it
    is recommended to review the designs at a later stage.

---
+++ app/src/Event/EventFormType.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Event;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Form used to render and validate the submission of a new event.
+ *
+ * Usage (extraneous use of variables is made to illustrate which parts are used):
+ *
+ * ```
+ * $formType = new EventFormType();
+ * $factory  = $this->application->formFactory;
+ * $form     = $factory->create($formType);
+ * $formName = $form->getName();
+ *
+ * if ($this->application->request()->isPost()) {
+ *     $data = $request->post($formName);
+ *
+ *     $form->submit($data);
+ *
+ *     if ($form->isValid()) {
+ *         // ... perform success actions
+ *     }
+ * }
+ * ```
+ */
+class EventFormType extends AbstractType
+{
+    /**
+     * Returns the name of this form type.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'event';
+    }
+
+    /**
+     * Adds fields with their types and validation constraints to this definition.
+     *
+     * This method is automatically called by the Form Factory builder and does not need
+     * to be called manually, see the class description for usage information.
+     *
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     *
+     * @return void
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', array('constraints' => array(new Assert\NotBlank(), new Assert\Length(array('min' => 5)))))
+            ->add('description', 'textarea', array('constraints' => array(new Assert\NotBlank(), new Assert\Length(array('min' => 5)))))
+            ->add(
+                'timezone',
+                'choice',
+                array('choices' => $this->getListOfTimezones(), 'constraints' => array(new Assert\NotBlank()))
+            )
+            ->add('start_date', 'date', $this->getOptionsForDateWidget('Start'))
+            ->add('end_date', 'date', $this->getOptionsForDateWidget('End'))
+            ->add('href', 'url', $this->getOptionsForUrlWidget('Website'))
+            ->add('cfp_start_date', 'date', $this->getOptionsForDateWidget('Call for Papers start', false))
+            ->add('cfp_end_date', 'date', $this->getOptionsForDateWidget('Call for Papers end', false))
+            ->add('cfp_url', 'url', $this->getOptionsForUrlWidget('Call for Papers website', false))
+        ;
+    }
+
+    /**
+     * Returns a series of options specific to the field of type 'URL'.
+     *
+     * To properly display a field where a URL can be entered we need to:
+     *
+     * - Validate it so that the URL is not malformed.
+     * - Show a placeholder in the input that demonstrates the format.
+     * - Display the right label.
+     * - when required add the validation that ensures the field is not empty.
+     *
+     * @param string  $label
+     * @param boolean $required
+     *
+     * @return string[]
+     */
+    private function getOptionsForUrlWidget($label, $required = true)
+    {
+        $constraints = array(new Assert\Url());
+        if ($required) {
+            $constraints[] = new Assert\NotBlank();
+        }
+
+        return array(
+            'label'    => $label,
+            'required' => $required,
+            'attr'     => array('placeholder' => 'http://example.org'),
+            'constraints' => $constraints
+        );
+    }
+
+    /**
+     * Returns a series of options specific to the field of type 'date'.
+     *
+     * To properly display a field where a URL can be entered we need to:
+     *
+     * - Validate it so that the date matches Y-m-d.
+     * - Force the widget to be rendered as a HTML5 'date' input.
+     * - Display the right label.
+     * - when required add the validation that ensures the field is not empty.
+     *
+     * @param string  $label
+     * @param boolean $required
+     *
+     * @return string[]
+     */
+    private function getOptionsForDateWidget($label, $required = true)
+    {
+        $constraints = array(new Assert\Date());
+        if ($required) {
+            $constraints[] = new Assert\NotBlank();
+        }
+
+        return array(
+            'label'    => $label,
+            'required' => $required,
+            'widget'   => 'single_text', // force date widgets to show a single HTML5 'date' input
+            'constraints' => $constraints
+        );
+    }
+
+    /**
+     * Returns an associative array with timezones.
+     *
+     * Both the key and value contain the name of the timezone so that the select box will pass a string value and
+     * not a numeric value. Although PHP recognizes 'UTC' as timezone we explicitly remove that because it does not
+     * fit with the Joind.in API.
+     *
+     * @return string[]
+     */
+    public function getListOfTimezones()
+    {
+        $timezones = \DateTimeZone::listIdentifiers();
+        array_pop($timezones); // Remove UTC from the end of the list
+
+        $result = array();
+        foreach($timezones as $timezone) {
+            $result[$timezone] = $timezone;
+        }
+
+        return $result;
+    }
+
+} 
