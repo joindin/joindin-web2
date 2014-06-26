61563a737dbece73b277fe3198f5225d82a27c23
Justin Carmony <justin@justincarmony.com>
2014-06-26 21:35:51 +0200

    Fixing code style violations

--- app/src/Application/Config.php
+++ app/src/Application/Config.php
@@ -6,21 +6,25 @@ class Config implements \ArrayAccess
 {
     protected $settings = array();
 
-    public function __construct($settings){
+    public function __construct($settings)
+    {
         $this->settings = $settings;
     }
 
-    public function offsetSet($key, $value) {
+    public function offsetSet($key, $value)
+    {
         throw new \Exception("You cannot set a config value!");
     }
-    public function offsetExists($key) {
+    public function offsetExists($key)
+    {
         return isset($this->settings[$key]);
     }
-    public function offsetUnset($offset) {
+    public function offsetUnset($offset)
+    {
         throw new \Exception("You cannot unset a config value!");
     }
-    public function offsetGet($key) {
-
+    public function offsetGet($key)
+    {
         return array_key_exists($key, $this->settings) ? $this->settings[$key] : null;
     }
 }
