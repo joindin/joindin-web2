cb901cc808820eb68f74b4b648cdfcb2dbba9aca
Kazushige Tominaga <kazu9su@gmail.com>
2015-03-28 02:47:21 +0900

    JOINDIN-564 Logout should leave me on the same page

--- app/src/User/UserController.php
+++ app/src/User/UserController.php
@@ -89,11 +89,11 @@ class UserController extends BaseController
             }
         }
         $this->render(
-		'User/login.html.twig',
-		array(
-		    'redirect' => $request->get('redirect'),
-		)
-	    );
+	    'User/login.html.twig',
+	    array(
+	        'redirect' => $request->get('redirect'),
+	    )
+	);
     }
 
     /**
