2f608d893ca76d511ab3f3fd99f8d071ced9700e
寺田 <wataru.terada@gmo-research.jp>
2015-09-28 07:05:43 +0900

    JOINDIN-603 Localization of the only Talk Comment page See: https://joindin.jira.com/browse/JOINDIN-603

---
+++ app/templates/Talk/lang/ja_JP.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'comments' => 'コメント',
+    'write_a_comment' => 'コメントを記入してください',
+    'rating_1' => '面白くなかった',
+    'rating_2' => '見込みあり',
+    'rating_3' => '聞いて良かった',
+    'rating_4' => 'すぐれた情報・洞察だった',
+    'rating_5' => '実に見事！ すばらしい！',
+    'rate_the_talk' => '評価',
+    'your_comment' => 'コメント',
+    'submit_comment' => 'コメント送信',
+    'comments_are_closed' => '現在、コメントは受け付けておりません。',
+    'please_log_in' => 'このイベントにコメントする場合はログインしてください。',
+);
