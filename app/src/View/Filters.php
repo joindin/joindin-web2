bec307ee7f31859db02faebaadb66c1f9a3429e8
Rob Allen <rob@akrabat.com>
2015-12-13 21:45:41 +0000

    Configure base URL to images
    This allows for setting the Base URL of images within the configuration
    file so that they are visible during development.

--- app/src/View/Filters.php
+++ app/src/View/Filters.php
@@ -3,12 +3,22 @@ namespace View\Filters;
 
 use Twig_Environment;
 use Twig_Filter_Function;
+use Slim\Slim;
 
-function initialize(Twig_Environment $env)
+function initialize(Twig_Environment $env, Slim $app)
 {
     $env->addFilter(
         'img_path',
-        new Twig_Filter_Function('\View\Filters\img_path', ['needs_environment' => true])
+        new Twig_Filter_Function(
+            function ($env, $suffix, $infix) use ($app) {
+                $base_url = $app->config('image_base_url');
+                if (!$base_url) {
+                    $base_url = 'https://joind.in/inc';
+                }
+                return \View\Filters\img_path($env, $suffix, $infix, $base_url);
+            },
+            ['needs_environment' => true]
+        )
     );
     $env->addFilter(
         'link',
@@ -20,7 +30,7 @@ function initialize(Twig_Environment $env)
     $env->addFilter('format_date', new Twig_Filter_Function('\View\Filters\format_date'));
 }
 
-function img_path($env, $suffix, $infix)
+function img_path($env, $suffix, $infix, $base_url)
 {
     if (!$suffix && $infix = 'event_icons') {
         $suffix = 'none.png';
@@ -34,7 +44,7 @@ function img_path($env, $suffix, $infix)
         return $uri . $path;
     }
 
-    return 'https://joind.in/inc' .$path;
+    return $base_url .$path;
 }
 
 function format_date($date)
