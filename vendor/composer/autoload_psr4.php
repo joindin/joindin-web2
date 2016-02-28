117eba018bc3de1d8626de2030dae318c2e8a654
Lorna Jane Mitchell <lorna@lornajane.net>
2016-02-28 21:46:41 +0000

    Guzzle dependency files
    Composer.lock also has more changes, I think due mostly to repo renaming

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,10 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
     'Org_Heigl\\DateRange\\' => array($vendorDir . '/org_heigl/daterange/src'),
     'Org_Heigl\\DateRangeTest\\' => array($vendorDir . '/org_heigl/daterange/tests'),
+    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
+    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
+    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
 );
