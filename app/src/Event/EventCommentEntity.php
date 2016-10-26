2f95a0b231f761d5ea20701a36f9553fec696ada
Eric Poe <eric.poe@gmail.com>
2016-10-26 07:33:23 -0500

    Stylistically match where a property comes from in the ctor

--- app/src/Event/EventCommentEntity.php
+++ app/src/Event/EventCommentEntity.php
@@ -9,6 +9,8 @@ class EventCommentEntity extends BaseCommentEntity
     public function __construct(stdClass $data)
     {
         parent::__construct($data);
-        $this->commentUri = isset($data->comment_uri) ? $data->comment_uri : null;
+        if (isset($this->data->comment_uri)) {
+            $this->commentUri = $this->data->comment_uri;
+        }
     }
 }
