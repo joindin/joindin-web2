d2e16a1ab8834f85061b9d41a771d37e5b759f42
Clair Shaw <clair@clairshaw.co.uk>
2017-03-22 20:34:40 +0000

    Introduce SASS to the website

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
 'use strict';
 
-module.exports = function(grunt) {  
+module.exports = function(grunt) {
       grunt.loadNpmTasks('grunt-jsvalidate');
+      grunt.loadNpmTasks('grunt-contrib-sass');
 
       grunt.initConfig({
         jsvalidate: {
@@ -15,8 +16,21 @@ module.exports = function(grunt) {
               src:['web/js/**/*.js']
             }
           }
+        },
+        sass: {
+          options:{
+            'loadPath': 'sass',
+            'style': 'compressed'
+          },
+          dist: {
+              files: [{
+                expand: true,
+                cwd: 'scss',
+                src: ['main.scss'],
+                dest: 'web/css/',
+                ext: '.css'
+              }]
+            }
         }
       });
-
 };
-
