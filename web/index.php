db91ff2a20f6bbdea4d26f53cc45a31768320a91
Scott Dutton <exussum12@users.noreply.github.com>
2019-01-19 15:31:11 +0000

    Stop the gc logging out
    Note, This will massively increase the amount of disk space sessions take up. Though I still can't imagine that being > 2 GB or so

--- web/index.php
+++ web/index.php
@@ -10,7 +10,9 @@ if (in_array(substr($_SERVER['REQUEST_URI'], -4), ['.css', '.jpg', '.png'])) {
 // include dependencies
 require '../vendor/autoload.php';
 
-session_set_cookie_params(60*60*24*7); // One week cookie
+$sessionLife = 60*60*24*7; // One week
+session_set_cookie_params($sessionLife);
+ini_set('session.gc_maxlifetime', $sessionLife);
 session_cache_limiter(false);
 session_start();
 
