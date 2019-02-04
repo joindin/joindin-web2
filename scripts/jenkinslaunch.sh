30979fa674007a880952fb495df39b2fcbff34a4
MichaelC <m@michaelcullum.com>
2019-02-04 18:39:21 +0000

    Don't cd for composer install, use --working-dir

--- scripts/jenkinslaunch.sh
+++ scripts/jenkinslaunch.sh
@@ -34,10 +34,9 @@ mkdir -p $TARGET \
 && git archive $LAUNCHREF | tar xC $TARGET \
 && (echo $TARGET ; echo $LAUNCHREF) > $TARGET/web/release.txt \
 && ln -s $TARGETBASE/config.php $TARGET/config/config.php \
-&& cd $TARGET && composer install --no-dev -o \
+&& composer install -o --prefer-dist --no-dev --no-progress --working-dir=$TARGET \
 && ln -s $TARGET $TARGETBASE/www.new \
 && mv -Tf $TARGETBASE/www.new $TARGETBASE/www \
 && rm -rf /tmp/joindin-twig-cache/live \
 && rm -rf /tmp/joindin-twig-cache/test
 "
-
