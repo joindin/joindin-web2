9ba883397604383fe086528730386d500b5c8ba0
Jos Elstgeest <zawadi@gmail.com>
2019-06-09 14:50:05 +0200

    fix test

--- tests/Event/EventFormTypeTest.php
+++ tests/Event/EventFormTypeTest.php
@@ -2,7 +2,6 @@
 
 namespace JoindIn\Web\Tests\Event;
 
-use JoindIn\Web\Event\EventEntity;
 use JoindIn\Web\Event\EventFormType;
 use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
 use Symfony\Component\Form\Form;
@@ -32,7 +31,7 @@ class EventFormTypeTest extends TypeTestCase
 
     public function testSubmitValidData()
     {
-        $formData = [
+        $formResultData = [
             "name"           => "Community Conference",
             "description"    => "Conference description.",
             "tags"           => ["php", "conference", "web"],
@@ -50,18 +49,36 @@ class EventFormTypeTest extends TypeTestCase
             "new_icon"       => null
         ];
 
+        $submittedFormData = [
+            "name"           => "Community Conference",
+            "description"    => "Conference description.",
+            "tags"           => "php, conference, web",
+            "tz_continent"   => "Europe",
+            "tz_place"       => "Amsterdam",
+            "start_date"     => "2019-01-01",
+            "end_date"       => "2019-01-02",
+            "href"           => "https://example.com/2019",
+            "cfp_start_date" => "2019-01-03",
+            "cfp_end_date"   => "2019-01-01",
+            "cfp_url"        => "https://example.com/cfp",
+            "location"       => "Location",
+            "latitude"       => "52.3546273",
+            "longitude"      => "4.8284121",
+            "new_icon"       => null
+        ];
+
         $form = $this->factory->create(EventFormType::class);
 
-        $form->submit($formData);
+        $form->submit($submittedFormData);
 
         $this->assertTrue($form->isSynchronized());
 
-        $this->assertEquals($formData, $form->getData());
+        $this->assertEquals($formResultData, $form->getData());
 
         $view     = $form->createView();
         $children = $view->children;
 
-        foreach (array_keys($formData) as $key) {
+        foreach (array_keys($formResultData) as $key) {
             $this->assertArrayHasKey($key, $children);
         }
     }
