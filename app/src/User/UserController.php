6cf5fc597a91f7ea0238ad53e9657261573641d6
Joe Ferguson <joe@joeferguson.me>
2019-10-27 01:02:56 -0500

    Fixing PHPStan level 1 issuesd in #883

--- app/src/User/UserController.php
+++ app/src/User/UserController.php
@@ -245,6 +245,7 @@ class UserController extends BaseController
         $talkDb   = $this->getTalkDb();
         $talkApi  = $this->getTalkApi();
         $eventApi = $this->getEventApi();
+        $eventUri = null;
 
         $eventInfo = []; // look up an event's name and url_friendly_name from its uri
         $talkInfo  = []; // look up a talk's url_friendly_talk_title from its uri
@@ -443,6 +444,7 @@ class UserController extends BaseController
         $talkDb       = $this->getTalkDb();
         $talkApi      = $this->getTalkApi();
         $eventApi     = $this->getEventApi();
+        $eventUri     = null;
         $talkComments = $talkApi->getComments($user->getTalkCommentsUri(), true, 0);
         if (!$talkComments) {
             $this->application->redirect($this->application->urlFor('user-profile', ['username' => $username]));
