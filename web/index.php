8c9bcf24e162adbcad9ab635e855fe89ea2c4f2e
Ian Littman <iansltx@gmail.com>
2020-01-24 06:59:34 -0600

    Bail out more carefully when Slim asks the app to halt
    e.g. when redirecting as a result of the trailing slashes middleware;
    this should allow the normal app life cycle to operate on a redirect
    or the like rather than throwing an exception, turning a redirect into
    a 500.

--- web/index.php
+++ web/index.php
@@ -93,6 +93,16 @@ $app->notFound(function () use ($app) {
 // register middlewares
 $app->add(new Middleware\ValidationMiddleware());
 $app->add(new Middleware\TrailingSlashMiddleware());
+$app->add(new class extends \Slim\Middleware { // catch Stops thrown in middlewares so they don't get thrown all the way
+    public function call()
+    {
+        try {
+            $this->next->call();
+        } catch (\Slim\Exception\Stop $e) {
+            $this->app->response()->write(ob_get_clean());
+        }
+    }
+});
 
 $csrfSecret = null;
 if (!empty($config['slim']['custom']['csrfSecret'])) {
@@ -175,4 +185,8 @@ new Client\ClientController($app);
 new Apikey\ApikeyController($app);
 
 // execute application
-$app->run();
+try {
+    $app->run();
+} catch (\Slim\Exception\Stop $e) {
+
+}
