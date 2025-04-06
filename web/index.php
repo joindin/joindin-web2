213894fab04da13b90597a680b439803a95632f1
Sander Bol <s.bol@egeniq.com>
2025-04-06 16:33:55 +0200

    Disable deprecation warnings temporarily

--- web/index.php
+++ web/index.php
@@ -40,7 +40,7 @@ $app = new \Slim\Slim(
 );
 
 $app->configureMode('development', function () use ($app) {
-    error_reporting(-1);
+    error_reporting(E_ALL & ~E_DEPRECATED);
     ini_set('display_errors', 1);
     ini_set('html_errors', 1);
     ini_set('display_startup_errors', 1);
